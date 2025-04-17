1f86144fbe6a2ff7d1621cfe33571b1279b04fc4
Logan Vasquez <logan@datafleet.com>
2025-04-17 18:22:09 -0500

    Adjust debounce implementation

--- lua/cmp_ai/source.lua
+++ lua/cmp_ai/source.lua
@@ -4,10 +4,38 @@ local conf = require('cmp_ai.config')
 local utils = require('cmp_ai.utils')
 
 local Source = {}
+
+-- Store completion context and timer
+local completion_state = {
+  timer = nil,
+  self = nil,
+  ctx = nil,
+  callback = nil
+}
+
 function Source:new(o)
   o = o or {}
   setmetatable(o, self)
   self.__index = self
+  
+  -- Create autocommands for cleanup and mode handling
+  local group = api.nvim_create_augroup("CmpAICompletion", { clear = true })
+  
+  -- Stop completion when leaving insert mode
+  api.nvim_create_autocmd("InsertLeave", {
+    group = group,
+    callback = function()
+      if completion_state.timer then
+        completion_state.timer:stop()
+        completion_state.timer:close()
+        completion_state.timer = nil
+      end
+      completion_state.self = nil
+      completion_state.ctx = nil
+      completion_state.callback = nil
+    end,
+  })
+  
   return o
 end
 
@@ -16,19 +44,21 @@ function Source:get_debug_name()
 end
 
 function Source:_do_complete(ctx, cb)
+  -- Only proceed if we're still in insert mode
+  if vim.fn.mode() ~= 'i' then
+    return
+  end
+
   if conf:get('notify') then
     conf:get('notify_callback')('Completion started')
   end
+  
   local max_lines = conf:get('max_lines')
   local cursor = ctx.context.cursor
   local cur_line = ctx.context.cursor_line
-  -- properly handle utf8
-  -- local cur_line_before = string.sub(cur_line, 1, cursor.col - 1)
+  
   local cur_line_before = vim.fn.strpart(cur_line, 0, math.max(cursor.col - 1, 0), true)
-
-  -- properly handle utf8
-  -- local cur_line_after = string.sub(cur_line, cursor.col) -- include current character
-  local cur_line_after = vim.fn.strpart(cur_line, math.max(cursor.col - 1, 0), vim.fn.strdisplaywidth(cur_line), true) -- include current character
+  local cur_line_after = vim.fn.strpart(cur_line, math.max(cursor.col - 1, 0), vim.fn.strdisplaywidth(cur_line), true)
 
   local lines_before = api.nvim_buf_get_lines(0, math.max(0, cursor.line - max_lines), cursor.line, false)
   table.insert(lines_before, cur_line_before)
@@ -41,9 +71,6 @@ function Source:_do_complete(ctx, cb)
   local service = conf:get('provider')
   service:complete(before, after, function(data)
     self:end_complete(data, ctx, cb)
-    if conf:get('notify') then
-      conf:get('notify_callback')('Completion started')
-    end
   end)
 end
 
@@ -53,14 +80,38 @@ function Source:complete(ctx, callback)
     callback()
     return
   end
+
+  -- Store completion context
+  completion_state.self = self
+  completion_state.ctx = ctx
+  completion_state.callback = callback
+
+  -- Clear existing timer if any
+  if completion_state.timer then
+    completion_state.timer:stop()
+    completion_state.timer:close()
+  end
+
+  -- Create new timer
+  completion_state.timer = vim.loop.new_timer()
   
-  -- Generate a unique key for this buffer
-  local debounce_key = string.format('cmp_ai_debounce_%d', api.nvim_get_current_buf())
-  
-  -- Wrap the completion call in a debounce
-  utils.debounce(debounce_key, function()
-    self:_do_complete(ctx, callback)
-  end, conf:get('debounce_ms'))
+  -- Start timer with debounce delay
+  completion_state.timer:start(
+    conf:get('debounce_delay'),
+    0,
+    vim.schedule_wrap(function()
+      -- Only proceed if we still have valid completion context
+      if completion_state.self and completion_state.ctx and completion_state.callback then
+        self:_do_complete(completion_state.ctx, completion_state.callback)
+      end
+      
+      -- Cleanup timer
+      if completion_state.timer then
+        completion_state.timer:close()
+        completion_state.timer = nil
+      end
+    end)
+  )
 end
 
 function Source:end_complete(data, ctx, cb)
